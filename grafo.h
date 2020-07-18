a2402a44a6f918c410c06a1a1064af2c674b05ef
CarlaNunes <carla.nunes@usp.br>
2020-07-18 14:23:42 -0300

    Versão atualizada com que foi entendido.
    Sujeita a modificações.

--- grafo.h
+++ grafo.h
@@ -2,21 +2,13 @@
 #define NULO NULL
 
 
-typedef struct{
-    char peso[30];//O nome do filme
-    struct taresta *prox;
-}taresta;
 
 typedef struct{
-    char nome[15];
-    char sobrenome[15];
-}tvertice;
-
-typedef taresta* tapontador;
-
-typedef struct{
-    tapontador vet[MAXNUMVERTICES];
     int num_vertices;
-
+    char vertNomeFilme[45];//Vai na matriz peso
+    int grau_max;
+    char **arestas;//Matriz com o linha vertice vai o nome do ator  e a coluna com quem ele liga;
+    char **peso;//Matriz que vai o nome do filme e com quem ele liga(ator);
+    char nomeCompleto[65];//Esse valor vai para os vertices;
 }tgrafo;
 

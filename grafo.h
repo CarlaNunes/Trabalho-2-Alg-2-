3c2e279c8913fa930e2cc61797219be540b289e1
CarlaNunes <carla.nunes@usp.br>
2020-07-18 19:22:11 -0300

    Grafa, pensado por hora.
    Sujeito a modificações.

--- grafo.h
+++ grafo.h
@@ -1,14 +1,20 @@
-#define MAXNUMVERTICES 100
-#define NULO NULL
+#include<stdio.h>
+#include<stdlib.h>
 
+typedef struct vertice{
+    char nomeCompleto[30];
+    struct vertice *proximo;
+    taresta aresta;
+}tvertice;
 
+typedef struct aresta{
+    char nomeFilme[60];
+    struct aresta *proxima;
+    struct aresta *anterior;
+}taresta;
 
 typedef struct{
     int num_vertices;
-    char vertNomeFilme[45];//Vai na matriz peso
-    int grau_max;
-    char **arestas;//Matriz com o linha vertice vai o nome do ator  e a coluna com quem ele liga;
-    char **peso;//Matriz que vai o nome do filme e com quem ele liga(ator);
-    char nomeCompleto[65];//Esse valor vai para os vertices;
+    tvertice vertice;
 }tgrafo;
 

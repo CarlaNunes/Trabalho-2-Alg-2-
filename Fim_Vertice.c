2bae6828833f8a6b899a374b589b1892a00dd427
CarlaNunes <carla.nunes@usp.br>
2020-07-19 13:08:19 -0300

    Create Fim_Vertice.c
    Por favor, verificar se está correta.

---
+++ Fim_Vertice.c
@@ -0,0 +1,18 @@
+//Versão para coreção
+
+#include<stdio.h>
+
+#include "grafo.h"
+
+
+char Fim_Vertice(tgrafo *grafo,taresta aresta, int num_vertice){
+    tvertice vertice;
+    if((grafo->num_vertices =  num_vertice) ){
+       for(int i; i < 2;i++){
+            return vertice->nomeCompleto;
+       }
+    }
+    else{
+        return NULL;
+    }
+}
